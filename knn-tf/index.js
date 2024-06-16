7078635ff067b6cc06da5d532b2d7a2b4f244a29
Jian Huang <jianhuang16888@gmail.com>
2024-06-16 16:36:32 -0400

    Add comments

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -8,7 +8,8 @@ about:Inspect
 */
 
 function knn(features, labels, predictionPoint, k) {
-    const {mean, variance} = tf.moments(features, 0);
+    const {mean, variance} = tf.moments(features, 0); // along 0 (column axis)
+    // (value - mean) / sq root of variance
     const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(.5));
 
     return 	features
@@ -16,13 +17,13 @@ function knn(features, labels, predictionPoint, k) {
         .div(variance.pow(.5))
         .sub(scaledPrediction)
         .pow(2)
-        .sum(1)
-        .pow(.5)
-        .expandDims(1)
-        .concat(labels, 1)
-        .unstack()
-        .sort((a,b) => a.get(0) > b.get(0)? 1:-1)
-        .slice(0, k)
+        .sum(1)  // 1 axis, and reduce dimension, become 1D
+        .pow(.5) // distance of M dimensions
+        .expandDims(1) // expand Dims into 2D, shape [n,1]
+        .concat(labels, 1) 
+        .unstack() // convert into tensor array
+        .sort((a,b) => a.get(0) > b.get(0)? 1:-1) // get value from tensor
+        .slice(0, k) 
         .reduce((acc,pair) => acc + pair.get(1), 0)
 		    / k; 
 }
