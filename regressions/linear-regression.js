914b5421117fce6217ddbb14549035a15fcbd34d
Jian Huang <jianhuang16888@gmail.com>
2024-06-19 19:44:06 -0400

    linear regression. add train and test methods

---
+++ regressions/linear-regression.js
@@ -0,0 +1,53 @@
+const tf = require('@tensorflow/tfjs');
+const _ = require('lodash');
+
+class LinearRegression {
+    constructor(features, labels, options) {
+        this.features = tf.tensor(features);
+        this.labels = tf.tensor(labels);
+
+        this.features = tf
+            .ones([this.features.shape[0], 1]) // 2D tensor with ones
+            .concat(this.features, 1);
+
+        this.options = Object.assign(
+            { learningRate: 0.1, iterations: 1000 }, 
+            options
+        ); 
+
+        this.weights = tf.zeros([2, 1]);
+    }
+
+    gradientDescent() {
+        // transposed-features * ((features-w-ones * weights) - labels)
+        const currentGuesses = this.features.matMul(this.weights);
+        const differences = currentGuesses.sub(this.labels);
+        const slopes = this.features
+            .transpose()
+            .matMul(differences)
+            .div(this.features.shape[0]);
+
+        this.weights = this.weights
+            .sub(slopes.mul(this.options.learningRate));
+               
+    }    
+    
+    train() {
+        for (let i = 0; i < this.options.iterations; i++ ){
+            this.gradientDescent();
+        }
+    }  
+    
+    test(testFeatures, testLabels) {
+        testFeatures = tf.tensor(testFeatures);
+        testLabels = tf.tensor(testLabels);
+
+        testFeatures = tf.ones([testFeatures.shape[0], 1])
+            .concat(testFeatures, 1);
+
+        const predictions = testFeatures.matMul(this.weights);
+        predictions.print();
+    }
+}
+
+module.exports = LinearRegression;
